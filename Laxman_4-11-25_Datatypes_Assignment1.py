d84ef6207a945aded1e09d867e3d8449f1351e27
KonukantiLaxman <kl752008@gmail.com>
2025-11-06 20:12:28 +0530

    file updated

---
+++ Laxman_4-11-25_Datatypes_Assignment1.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Q1. Write a Python program to print the number 10.
+a=10
+print(a)
+
+
+# In[5]:
+
+
+#2. Write a program to print the numbers 5, 10, and 15 in a single line separated by spaces.
+a,b,c=5,10,15
+print(a, b, c)
+
+
+# In[6]:
+
+
+#3. Store your name in a variable and print it.
+a="Laxman"
+print(a)
+
+
+# In[9]:
+
+
+#4. Store your first name and last name in two variables and print both.
+a="Laxman"
+b="Konukati"
+print(a, b)
+
+
+# In[10]:
+
+
+#5. Store your age in a variable and print: I am 25 years old.
+a=23
+print('I am',a, 'years old')
+
+
+# In[19]:
+
+
+#6. Store your name and city in variables and print the message: My name is Ravi and I live in Mumbai.
+
+name="Laxman"
+city="Karimnagar"
+message="My name is {} and i live in {}" .format(name,city)
+print(message)
+
+
+# In[15]:
+
+
+#7. Print variables a = 10, b = 20, c = 30 separated by |.
+a=10
+b=20
+c=30
+print(a,b,c, sep="|")
+
+
+# In[20]:
+
+
+#8. Print Python and Rocks! on the same line without using two print() statements.
+a="Python"
+b="Rocks!"
+print(a, b)
+
+
+# In[21]:
+
+
+#9. Store two numbers 5 & 8 and print their sum using an f-string.
+a=5
+b=8
+print(f"sum is {a+b}")
+
+
+# In[22]:
+
+
+#10. Ask the user to enter their name and print: Hello, SHNA(SRK
+name="Laxman"
+message="hello, {}".format(name)
+print(message)
+
+
+# In[23]:
+
+
+#11. Create a variable named city and assign your favourite city name to it. Print teh variable
+city="Hyderabad"
+print(city)
+
+
+# In[25]:
+
+
+#12. Assign the value 10 to variable a and the value 20 to variable b. Swap the values of a and b and print
+         
+a=10
+b=20
+a,b=b,a
+print(a, b)
+
+
+# In[26]:
+
+
+#13. Create three variables in one line and assign them the values 5, 10, and "Python".
+a,b,c=5,10,"python"
+print(a, b, c)
+
+
+# In[27]:
+
+
+#14. Assign the same value "Red" to three variables color1, color2, and color3.
+color1="Red"
+color2="Red"
+color3="Red"
+print(color1, color2, color3)
+
+
+# In[49]:
+
+
+#15.Check the type of variable num that has value 3.14 and print it.
+num=3.14
+print(num,type(num))
+
+
+# In[30]:
+
+
+#16.Create a variable name with your name, and another variable age with your age. Print a sentence like:
+
+name="Laxman"
+age=23
+message="My name is {} and iam {} years old".format(name,age)
+print(message)
+
+
+# In[32]:
+
+
+#17. What will be the output of the following code?
+    
+a="5"
+b=3
+print(a*b)
+
+
+# In[33]:
+
+
+#18. Create a variable is_python_fun and assign it the boolean value True. Print it.
+_python_fun=True
+print(_python_fun)
+
+
+# In[34]:
+
+
+#19. What is the output of the x?
+
+x=10
+x+=5
+print(x)
+
+
+# In[48]:
+
+
+#20. Can you assign the value 5 to three variables a, b, and c in one line? Then, modify only b to 10 without changing a and c.
+
+a,b,c=5,5,5
+b=a+c
+print(a, b, c)
+
+
+# In[37]:
+
+
+#21.Assign 0 to five variables in one line, then update the second
+a,b,c,d,e=0,0,0,0,0
+f=2
+b,f=f,b
+print(a ,b ,c ,d ,e)
+
+
+# In[38]:
+
+
+#23.Create two variables x = 5 and y = 7, and print their sum.
+x=5
+y=7
+sum=x+y
+print(sum)
+
+
+# In[39]:
+
+
+#24. Swap the values of two variables.
+a=10
+b=20
+a,b=b,a
+print(a, b)
+
+
+# In[47]:
+
+
+#25. Create a variable greeting with value "Hello" and print it.
+greeting='Hello'
+print(greeting)
+
+
+# In[46]:
+
+
+#26. Assign the result of 5 * 8 to a variable result and print it.
+a=5
+b=8
+result=a*b
+print(result)
+
+
+# In[42]:
+
+
+#27. Assign two values to two variables in one line.
+a,b=5,10
+print(a, b)
+
+
+# In[43]:
+
+
+#28. Assign your first name and last name to two variables, then print them in one line.
+a="Laxman"
+b="Konukati"
+print(a, b)
+
+
+# In[45]:
+
+
+#30. Assign a number to a variable num. Now, update num by adding 5 to it. Print the new value of num.
+num=10
+num+=5
+print(num)
+
+
+# In[ ]:
+
+
+
+
