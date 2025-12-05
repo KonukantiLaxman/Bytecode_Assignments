9de2188479baf470b7d627d98b4be72653a7cafe
Laxman Konukati <155511970+KonukantiLaxman@users.noreply.github.com>
2025-12-05 08:18:47 +0530

    Update Laxman_28-11-25_numpyassignments_11.py

--- Laxman_28-11-25_numpyassignments_11.py
+++ Laxman_28-11-25_numpyassignments_11.py
@@ -60,45 +60,37 @@
 
 
 # In[6]:
-
-
 # Assignment 1: Array Creation and Manipulation
 # Create a NumPy array of shape (5, 5) filled with random integers between 1 and 20. Replace all the elements in the third column with 1.
 # Create a NumPy array of shape (4, 4) with values from 1 to 16. Replace the diagonal elements with 0.
-print('==array of shape (5, 5) filled with random integers between 1 and 20==')
 import numpy as np
-import random 
-a=np.random.randint(1,20,[5,5])
+a=np.random.randint(1,21,25)
 print(a)
-print('==Replacing all the elements in the third column with 1.==')
-a[:,2]=1
-print(a)
-
-# Create a NumPy array of shape (4, 4) with values from 1 to 16. 
-print('==array of shape (4,4) filled with values from 1 and 16 ')
-b=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+b=np.reshape(a,(5,5))
 print(b)
-
-# Replace the diagonal elements with 0.
-print('==Replacing the diagonal elements with 0.==')
-for i in range(0,4):
-    for j in range(0,4):
-        if i==j:
-            b[i][j]=0
+b[:,3]=1
 print(b)
+c=list(range(1,17))
+print(c)
+c=np.reshape(c,(4,4))
+for i in range(4):
+    c[i][i] = 0
+print(c)
+
+
+
 
 
 # In[26]:
-
 
 # Assignment 2: Array Indexing and Slicing
 # Create a NumPy array of shape (6, 6) with values from 1 to 36. Extract the sub-array consisting of the 3rd to 5th rows and 2nd to 4th columns.
 # Create a NumPy array of shape (5, 5) with random integers. Extract the elements on the border.
 import numpy as np
-a=np.array([[1,2,3,4,5,6],[7,8,9,10,11,12],[13,14,15,16,17,18],[19,20,21,22,23,24],[25,26,27,28,29,30],[31,32,33,34,35,36]])
+a=np.arange(1,37).reshape(6,6)
 print(a)
-a([3:6:2,2:5:2])
-a
+print("sub-array consisting of the 3rd to 5th rows and 2nd to 4th columns\n",a[2:5,1:4])
+
 
 # Create a NumPy array of shape (5, 5) with random integers. Extract the elements on the border.
 a=np.random.randint(1,20)
@@ -141,16 +133,13 @@ print(a//b)
 
 
 # 2. Create a NumPy array of shape (4, 4) with values from 1 to 16. Compute the row-wise and column-wise sum.
-b=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
-print(b)
-sum1=0
-sum2=0
-for i in range(0,4):
-    for j in range(0,4):
-        sum1+=b[i]
-    print(sum1)
-    sum2+=b[j]
-print(sum2)
+import numpy as np
+b=np.arange(1,17).reshape(4,4)
+print("Array:\n",b)
+row_sum=np.sum(b,axis=1)
+print("row_sum:\n",row_sum)
+col_sum=np.sum(b,axis=0)
+print("column_sum:\n",col_sum)
         
 
 
